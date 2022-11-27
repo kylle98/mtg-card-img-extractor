958ce1a58813b824e059006395797e32c910e7fe
kylle98 <49073563+kylle98@users.noreply.github.com>
2022-11-27 18:52:34 +0100

    Fixed waiting for element

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import time
-
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-UsingBrowser = webdriver.Chrome(options=options)
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def getImgLink(url):
-
-    UsingBrowser.get(url)
-    time.sleep(1)
-
-    html = UsingBrowser.page_source
-
-    sourceCode = BeautifulSoup(html, "html.parser")
-    searchingResults = sourceCode.find('img', class_="card-img-top")
-
-    return searchingResults['src']
+    with usingBrowser as driver:
+        driver.get(url)
+        try:
+            waitingForLoad = WebDriverWait(driver, 5)
+            waitingForLoad.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.card-image-front[_ngcontent-serverApp-c57]')))
+            htmlCode = driver.page_source
+            bsElement = BeautifulSoup(htmlCode, "html.parser")
+            results = bsElement.find('img', class_="card-img-top")
+            return results['src']
+        finally:
+            driver.quit()
 
 if __name__ == "__main__":
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    usingBrowser = webdriver.Chrome(options=options)
+
     url = str(input("Enter the link from mtg.dragonshield.com "))
     print(getImgLink(url))
