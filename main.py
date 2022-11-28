21f8cc8efd4195227f8fec66b8fe0b7e99ade785
kylle98 <49073563+kylle98@users.noreply.github.com>
2022-11-28 01:22:50 +0100

    Optymalize update, changed variables

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def getImgLink(url):
+def getImgLink(url, usingBrowser):
     with usingBrowser as driver:
         driver.get(url)
         try:
-            waitingForLoad = WebDriverWait(driver, 5)
-            waitingForLoad.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.card-image-front[_ngcontent-serverApp-c57]')))
+            waitCondition = WebDriverWait(driver, 10)
+            waitCondition.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.card-image-front[_ngcontent-serverApp-c57]')))
             htmlCode = driver.page_source
-            bsElement = BeautifulSoup(htmlCode, "html.parser")
-            results = bsElement.find('img', class_="card-img-top")
-            if (results != None):
-                return results['src']
+            bsParser = BeautifulSoup(htmlCode, "html.parser")
+            result = bsParser.find('img', class_="card-img-top")
+            if (result != None):
+                if (result['src']):
+                    return result['src']
+                else:
+                    return "Link didn't found"
             else:
-                return "Element not found"
+                return None
         finally:
             driver.quit()
 
@@ -25,5 +28,6 @@ if __name__ == "__main__":
     options.add_argument('--headless')
     usingBrowser = webdriver.Chrome(options=options)
 
-    url = str(input("Enter the link from mtg.dragonshield.com "))
-    print(getImgLink(url))
+    url = str(input("Enter the link from mtg.dragonshield.com\n"))
+    print("Searching in progress...\n")
+    print(getImgLink(url, usingBrowser))
